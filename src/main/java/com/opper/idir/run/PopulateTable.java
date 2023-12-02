5276baa8ab45d1880a954fa5a2094ea3ea304dc7
Emanuele Trombella emanuele.trombella@ext.soprasteria.com <emanuele.trombella@ext.soprasteria.com>
2023-12-02 22:43:42 +0100

    Introduzione log ogni singolo script lanciato all'interno del file populate.sql

--- src/main/java/com/opper/idir/run/PopulateTable.java
+++ src/main/java/com/opper/idir/run/PopulateTable.java
@@ -4,11 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
@@ -22,7 +23,7 @@ public class PopulateTable {
 	private static Logger logger = LoggerFactory.getLogger(PopulateTable.class);
 
 	private Properties properties;
-	
+
 	private String pathFile;
 
 	public PopulateTable(String pathFile) throws IOException {
@@ -33,15 +34,21 @@ public class PopulateTable {
 
 	public void executeSqlScript(String fileName) throws Exception {
 
-		try(Connection conn = getConnection();){
+		try (Connection conn = getConnection();) {
 			File file = getScriptSqlFile(fileName);
 			try (Reader reader = new BufferedReader(new FileReader(file))) {
-				logger.info("Running script from file: " + file.getCanonicalPath());
-				ScriptRunner sr = new ScriptRunner(conn);
-				sr.setAutoCommit(true);
-				sr.setStopOnError(true);
-				sr.runScript(reader);
-				logger.info("Done.");
+				try (FileWriter logFile = new FileWriter("T:\\OPPER\\jar\\logs\\appOpperScriptRunner_"+file.getName()+".log");
+						PrintWriter output = new PrintWriter(logFile);) {
+					logger.info("Running script from file: " + file.getCanonicalPath());
+					OpperScriptRunner sr = new OpperScriptRunner(conn);
+					sr.setErrorLogWriter(output);
+					sr.setLogWriter(output);					
+					sr.setAutoCommit(true);
+					sr.setStopOnError(true);
+					sr.setSendFullScript(false);
+					sr.runScript(reader);
+					logger.info("Done.");
+				}
 			} catch (Exception ex) {
 				throw ex;
 			}
@@ -49,10 +56,11 @@ public class PopulateTable {
 			throw ex;
 		}
 	}
-	
+
 	private Connection getConnection() throws SQLException {
- 
-		return DriverManager.getConnection(properties.getProperty("datasource.jdbc-url"), properties.getProperty("datasource.username"), properties.getProperty("datasource.password"));
+
+		return DriverManager.getConnection(properties.getProperty("datasource.jdbc-url"),
+				properties.getProperty("datasource.username"), properties.getProperty("datasource.password"));
 	}
 
 	private void readPropertiesFile() throws IOException {
@@ -69,9 +77,9 @@ public class PopulateTable {
 			throw ex;
 		}
 	}
-	
+
 	private File getScriptSqlFile(String fileName) {
 
 		return new File(pathFile.concat(fileName));
-	}	
+	}
 }
